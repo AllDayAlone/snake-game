f45dc58c67b144c3cf7473df3babe691cf66a13b
Pavel <ptashka.kl@gmail.com>
2018-07-08 01:21:45 +0300

    Add fruit, end game if intersect itself or edge, interaction with fruits (grow and speed) , refactor setup with generation

--- src/sketch.js
+++ src/sketch.js
@@ -10,28 +10,86 @@ const cardinalDirections = {
 }
 
 let head = { x: ww/2, y: wh/2 };
-let velocity = 10;
-let snake = [];
-let dir = 'e';
-
+let velocity;
+let snake;
+let fruit;
+let dir;
+let lost;
 
 function setup() {
   createCanvas(ww, wh);
-  background(220, 230, 240);
-  for (let i = 0; i < initialLength; i++) {
-    snake.push({ x: ww/2, y: wh/2 + pixelSize * i });
-  }
+  noStroke();
+  createDiv("Press 'R' to restart");
+  startGame();
 }
 
 function draw() {
-	frameRate(velocity);
-	background(220, 230, 240);
-	noStroke();
+	if (lost) {
+        background('black');
+        return;
+	}
+	
+	frameRate(velocity);	
+	drawField();
+	
 	fill(51);	
 	snake.forEach(({x, y}) => rect(x, y, pixelSize, pixelSize));
+	
+	fill('red');
+	rect(fruit.x, fruit.y, 10, 10);	
+	
 	move();
-	velocity += 1;
+
+	if (snake[0].x === fruit.x && snake[0].y === fruit.y) {
+		velocity += 1;
+		generateFruit();
+		snake.push(Object.assign(fruit, {}));
+    }
+
+    checkIntersection();
 }
+
+function generateFruit() {
+    fruit = {};
+	fruit.x = floor(random(10, ww / 10)) * 10;
+	fruit.y = floor(random(10, wh / 10)) * 10;
+	for (let el of snake) {
+		if (el.x === fruit.x && el.y === fruit.y) {
+			generateFruit();
+			break;
+		}
+	}
+}
+
+
+function startGame() {
+    lost = 0;
+    dir = 'n';
+    velocity = 10;
+    generateSnake(initialLength);  
+    generateFruit();
+}
+
+function generateSnake(len) {
+    snake = [];
+    for (let i = 0; i < len; i++) {
+        snake.push({ x: ww/2, y: wh/2 + pixelSize * i });
+      }
+}
+
+function drawField() {
+    background(220, 230, 240);
+    fill('grey');
+    for (let j = 0; j < wh; j += 10) {
+        rect(0, j, 10, 10);
+        rect(ww - 10, j, 10, 10);
+    }
+    for (let i = 0; i < ww; i += 10) {
+        rect(i, 0, 10, 10);
+        rect(i, wh - 10, 10, 10);
+    }    
+}
+
 
 function move() {
 	let last = snake[snake.length - 1];
@@ -39,33 +97,41 @@ function move() {
 		? moveHead(snake[0], cardinalDirections[dir])
 		: snake[ind - 1]
 	);
-	if (random() < 0.3) snake.push(last);
 }
 
 function moveHead({ x, y }, { dirX, dirY }) {
-	let head = { x: x + dirX, y: y + dirY };
-	if (head.x < 0) head.x = ww;
-	if (head.x > ww) head.x = 0;
-	if (head.y < 0) head.y = wh;
-	if (head.y > wh) head.y = 0;
+	return  { x: x + dirX, y: y + dirY };
+}
 
-	return head;
+function checkIntersection() {
+	if (snake[0].x <= 0) lost = 1;
+	if (snake[0].x >= ww) lost = 1;
+	if (snake[0].y <= 0) lost = 1;
+    if (snake[0].y >= wh) lost = 1;
+    for (let i = 1; i < snake.length; i += 1) {
+        if (snake[0].x === snake[i].x && snake[0].y === snake[i].y) {
+            lost = 1;
+        }
+    }
+    
 }
 
 function keyPressed() {
-	console.log(keyCode);
 	switch(keyCode) {
 		case 37:
-			dir = 'w';
+			if (dir !== 'e') { dir = 'w'; }
 			break;
-		case 38:
-			dir = 'n';
+        case 38:
+            if (dir !== 's') { dir = 'n'; }
 			break;
-		case 39:
-			dir = 'e';
+        case 39:
+            if (dir !== 'w') { dir = 'e'; }
 			break;
-		case 40:
-			dir = 's';
-			break;
+        case 40:
+            if (dir !== 'n') { dir = 's'; }
+            break;
+        case 82:
+            startGame();
+            break;
 	}
 }
